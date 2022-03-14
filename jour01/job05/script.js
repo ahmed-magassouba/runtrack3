5639728d2adcc846c15e0c5c25bcf8ce9ca42cb8
ahmed sekou magassouba <ahmed.magassouba@laplateforme.io>
2022-03-14 11:11:36 +0100

    fin job5

---
+++ jour01/job05/script.js
@@ -0,0 +1,18 @@
+function afficherjourssemaine() {
+  var jourssemaine = [
+    "Lundi",
+    "Mardi",
+    "Mercredi",
+    "Jeudi",
+    "Vendredi",
+    "Samedi",
+    "Dimanche",
+  ];
+
+  for(jour of jourssemaine){
+    console.log(jour)
+  }
+}
+
+afficherjourssemaine()
+
