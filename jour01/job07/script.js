e74c3a3e05a529282703a12e230c3f4738f64178
Ahmed Sékou Magassouba <ahmed.magassouba@laplateforme.io>
2022-03-14 21:48:44 +0100

    fin job 7 et job 8

--- jour01/job07/script.js
+++ jour01/job07/script.js
@@ -1,34 +1,51 @@
 function jourtravaille(date) {
- 
+
+  let date_entrer = new Date(date);
+
+  let dateLocale = date_entrer.toLocaleString('fr-FR', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+  });
+
+  // console.log(dateLocale)
+
   jourferier = [
-    "01/01/2020",
-    "13/04/2020",
-    "01/05/2020",
-    "08/05/2020",
-    "21/05/2020",
-    "01/06/2020",
-    "14/07/2020",
-    "15/08/2020",
-    "01/11/2020",
-    "11/11/2020",
-    "25/12/2020",
+    new Date("2020/01/01"),
+    new Date("2020/04/13"),
+    new Date("2020/05/01"),
+    new Date("2020/05/08"),
+    new Date("2020/05/21"),
+    new Date("2020/06/01"),
+    new Date("2020/07/14"),
+    new Date("2020/08/15"),
+    new Date("2020/11/01"),
+    new Date("2020/11/11"),
+    new Date("2020/12/25")
   ];
 
+  // console.log(date_entrer.getTime())=> donne la valeur de la date en milliseconde
+  // console.log(date_entrer.getDay()) => donne le jour en chiffre de la date 
+
   for (jour of jourferier) {
-    if (date == jour) {
-      console.log(date + " est un jour ferié ");
+    if (date_entrer.getTime() == jour.getTime()) {
+      console.log("Le " + jour.getDate() + " " + jour.getMonth() + " " + jour.getFullYear() + " est un jour férié");
     }
   }
 
-  var enter = new Date(date);
- 
-console.log(enter.toLocaleDateString("fr"))
+  // var enter = new Date(date);
 
-  if (enter.getDay() == 0 || enter.getDay == 6) {
-    console.log("Non "+date + " est un week-end ");
-  }else{
-    console.log("Oui "+date + " est un jour travaillé "); 
+  // console.log(enter.toLocaleDateString("fr"))
+
+  if (date_entrer.getDay() == 0 || date_entrer.getDay == 6) {
+    console.log("Non, " + date_entrer.getDate() + " " + date_entrer.getMonth() + " " + date_entrer.getFullYear() + " est un week-end ");
+  } else {
+    console.log("Oui, " + date_entrer.getDate() + " " + date_entrer.getMonth() + " " + date_entrer.getFullYear() + " est un jour travaillé ");
   }
 }
 
-jourtravaille("5/10/2020");
+jourtravaille("2020/01/01");
--- jour01/job08/script.js
+++ jour01/job08/script.js
@@ -1,30 +1,28 @@
 function sommenombrespremiers(nbre1, nbre2) {
-    
-  if(nbre1 > 2) {
+
+  if (nbre1 > 2 && nbre2 > 2) {
+
+    var value1 = true
+    var value2 = true
 
     for (let i = 2; i < nbre1; i++) {
-
-      if (nbre1 % i == 0 ) {
-        console.log("niveau1");
+      if (nbre1 % i == 0) {
+        console.log(false);
+        value1 = false
       }
-          
     }
-
-
-  } else if(nbre2 > 2) {
 
     for (let j = 2; j < nbre2; j++) {
-
       if (nbre2 % j == 0) {
-        console.log("niveau2");
+        console.log("false");
+        value2 = false
       }
-
     }
 
-  } else {
-    console.log(nbre1 + nbre2);
+    if (value1 == true && value2 == true) {
+      console.log(nbre1 + nbre2);
+    }
   }
-  
 }
 
-sommenombrespremiers(5,5);
+sommenombrespremiers(3, 3);
