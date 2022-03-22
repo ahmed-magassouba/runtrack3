7ca4329623dee9f2ed7907d8486bc73a5561f817
ahmed sekou magassouba <ahmed.magassouba@laplateforme.io>
2022-03-22 15:24:13 +0100

    fin job 1

---
+++ jour07/index.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- Lien bootstarp 5 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <script rel="stylesheet" src="script.js"></script>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">LTPF</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-link active" aria-current="page" href="https://laplateforme.io/">Accueil</a>
+                        <a class="nav-link" href="#">Units</a>
+                        <a class="nav-link disabled" href="#">Jobs</a>
+                        <a class="nav-link disabled">Skils</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+        <div class="container-fluid  secondary pb-4">
+            <div class="row">
+                <h1 class="text-center display-6">LaPlateforme_</h1>
+            </div>
+            <div class="row gx-5">
+                <div class="col-3">
+                    <div class="card">
+                        <img src="12.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Un Papillon</h5>
+                            <p class="card-text m-0">Un papillon, c'est un peu comme une chenille, mais avec des ailes.
+                                <br>
+                                Ne pas ingerer.
+                            </p>
+
+                            <a href="#" class="btn btn-primary">Commander votre propre papillon</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-7 primary ">
+                    <div class="card-body m-5">
+                        <h1 class="display-6">Bonjour, monde!</h1>
+                        <p>Il existe plusieurs visions du terme:</p>
+                        <p>le monde est la matière, l'espace et les phénomènes qui nous sont accessibles par les sens, l'experience ou la raison.</p>
+                        <p>Le sens le plus courant désigne notre planète, la terre avec ses habitants, et son environnement plus ou moins naturel.</p>
+                        <hr>
+                        <p>Le sens étendu désigne l'univers dans son ensemble.</p>
+                        <a href="#" class="btn btn-danger">Rebooter le monde</a>
+
+                        <nav aria-label="Page navigation example">
+                            <ul class="pagination justify-content-end">
+                                <li class="page-item">
+                                    <a class="page-link" href="#" aria-label="Previous">
+                                        <span aria-hidden="true">&laquo;</span>
+                                    </a>
+                                </li>
+                                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                <li class="page-item">
+                                    <a class="page-link" href="#" aria-label="Next">
+                                        <span aria-hidden="true">&raquo;</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+
+                </div>
+                <div class=" col-2">
+
+                    <ul class="list-group fs-9">
+                        <li class="list-group-item active" aria-current="true">Luxure</li>
+                        <li class="list-group-item">Gourmandise</li>
+                        <li class="list-group-item">Avarice</li>
+                        <li class="list-group-item">Colère</li>
+                        <li class="list-group-item">Heresie</li>
+                        <li class="list-group-item">Violence</li>
+                        <li class="list-group-item">Ruse et Tromperie</li>
+                        <li class="list-group-item">Trahison</li>
+                        <li class="list-group-item">Internet Explorer</li>
+                    </ul>
+
+                </div>
+            </div>
+            <div class="row mt-4 ">
+                <div class="col-10">
+                    <h6 class="text-end">Instalation de AI 9000</h6>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="offset-3 col-7">
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-striped bg-warning" role="progressbar" style="width: 95%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="row m-4">
+                <div class="offset-1 col-4">
+                    <h6>Recevez votre copie gratuite d'internet 2!</h6>
+                    <div class="input-group flex-nowrap">
+                        <span class="input-group-text" id="addon-wrapping">@</span>
+                        <input type="text" class="form-control" placeholder="Login" aria-label="Username" aria-describedby="addon-wrapping">
+                    </div>
+                    <div class="input-group mt-4 mb-3">
+                        <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                        <span class="input-group-text" id="basic-addon2">@example.com</span>
+                    </div>
+                    <h6>URL des Internets 2 et 2.1 Beta</h6>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text">DogeCoin</span>
+                        <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
+                        <span class="input-group-text">.00</span>
+                    </div>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon3">https://l33t.lptf/dkwb/berlusconimkt/</span>
+                        <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+                    </div>
+                </div>
+                <div class="offset-2 col-3">
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                        <p>we'll never share your email with anyone else.</p>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Password</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                        <label class="form-check-label" for="flexCheckDefault">
+                            Check me out
+                        </label>
+                    </div>
+                    <button class="btn btn-primary">Submit</button>
+                </div>
+
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
